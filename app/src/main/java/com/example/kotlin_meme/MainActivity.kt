c0556a038108e0c39d2ea94e80928d48ee641132
Shobhit- Senpai <majin.shobhitrawat53@gmail.com>
2022-01-29 22:35:38 +0530

    lots of changes done

--- app/src/main/java/com/example/kotlin_meme/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_meme/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.kotlin_meme
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -40,7 +47,7 @@ class MainActivity : AppCompatActivity() {
     private fun shareMeme() {
         val sendIntent: Intent= Intent().apply {
             action= Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT,imageUrl)
+            putExtra(Intent.EXTRA_TEXT,"Hey check this Funny meme $imageUrl ")
             type="text/plain"
         }
         val shareIntent= Intent.createChooser(sendIntent,null)
@@ -48,11 +55,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun previousMeme() {
-        Glide.with(this).load(previousUrl).into(imageView)
+        rukoZara.visibility= View.VISIBLE
+
+        Glide.with(this).load(previousUrl).listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                rukoZara.visibility=View.GONE
+
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                rukoZara.visibility=View.GONE
+
+                return false
+            }
+
+        }).into(imageView)
     }
 
     private fun nextMeme() {
 
+        rukoZara.visibility= View.VISIBLE
         val queue= Volley.newRequestQueue(this)
         previousUrl=imageUrl
 
@@ -61,7 +95,31 @@ class MainActivity : AppCompatActivity() {
             { response ->
 
                 imageUrl=response.getString("url")
-                    Glide.with(this).load(imageUrl).into(imageView)
+                    Glide.with(this).load(imageUrl).listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            rukoZara.visibility=View.GONE
+                            return false
+
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            rukoZara.visibility=View.GONE
+                            return false
+
+                        }
+
+                    }).into(imageView)
 
             },
             { error ->
